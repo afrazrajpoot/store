6c807ab52b78c6c6af85b03cb8c993d1bf8c6daa
afrazrajpoot <afrazrajpoot46@gmail.com>
2025-04-26 01:13:24 +0500

    fix issue

--- src/app/(pages)/product/[...id]/page.jsx
+++ src/app/(pages)/product/[...id]/page.jsx
@@ -148,7 +148,7 @@ const ProductPage = ({ params }) => {
                       <AntImage
                         src={`/api/uploads?filename=${img}`}
                         alt={`${product.name} ${index + 1}`}
-                        className="w-full object-contain"
+                        className="w-[500px] h-[500px] object-contain"
                         preview={{
                           mask: (
                             <div className="text-[1vw] font-semibold">
